b18292cdf652b3666e2788b47740e08212faf05f
lijie <lijie1@supcon.com>
2021-01-19 20:31:46 +0800

    auto-configuire探究

---
+++ spring-boot-test2/src/main/java/com/steven/springboottest2/SpringBootTest2Application.java
@@ -0,0 +1,14 @@
+package com.steven.springboottest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class SpringBootTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTest2Application.class, args);
+    }
+
+}
